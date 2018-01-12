049b84797a8e3bae493bdbbe278be384eb3a0522
ferrannp <fnp.developer@gmail.com>
2018-01-12 15:15:50 +0100

    Eject create-react-app and use webpack.config.shared for web (to support aliases).

--- src/index.web.js
+++ src/index.web.js
@@ -2,9 +2,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import App from 'web/App';
+import registerServiceWorker from 'web/registerServiceWorker';
 import './index.css';
-import App from './web/App';
-import registerServiceWorker from './web/registerServiceWorker';
 
 /* $FlowFixMe */
 ReactDOM.render(<App />, document.getElementById('root'));
